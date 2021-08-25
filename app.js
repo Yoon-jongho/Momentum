84cb91f51af35120fd8d6578a19458ddc35a54c7
llsee01 <llsee01@naver.com>
2021-08-26 01:01:04 +0900

    Momentum setting

---
+++ app.js
@@ -0,0 +1,35 @@
+const loginForm = document.querySelector("#loginForm")
+const loginInput = document.querySelector("#loginForm input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USER_NAME_KEY = "userName";
+
+const paintGreetings = (userName) => {
+    greeting.innerText = `Hello! ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+
+const onLoginSubmit = (event) => {
+    event.preventDefault();   
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USER_NAME_KEY, userName);
+    paintGreetings(userName);
+}
+
+
+
+loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUserName = localStorage.getItem(USER_NAME_KEY);
+
+if(savedUserName === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUserName);
+}
+
