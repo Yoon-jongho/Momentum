f42380eb24fc5648403d41dc3c7a93ebbe4e367c
llsee01 <llsee01@naver.com>
2021-09-15 23:54:44 +0900

    JSON.parse , JSON.stringify , foreach

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,14 @@
 const toDoForm = document.getElementById("todoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+const saveToDos = () => { 
+   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 
 const deleteTodo = (e) => {
     const li = e.target.parentElement;
@@ -22,8 +30,18 @@ const paintToDo = (newTodo) => {
 const handleToDoSubmit = (event) => {
     event.preventDefault();
     const newTodo = toDoInput.value;
-    toDoInput.value = '';
+    toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY)
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
